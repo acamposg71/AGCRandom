e7ed9a03d64d5a99f60f9448ed15a66f1248e532
acamposg71 <acamposg71@gmail.com>
2015-07-13 15:29:58 -0500

    Update ACG_Random.js

--- ACG_Random.js
+++ ACG_Random.js
@@ -3,7 +3,7 @@ var numero = Math.random();
 
 var str = ' Mayor que 0.5';
 if (numero <= 0.5) {
-  console.log('\n' + numero + ' Menor que 0.5 \n');
+  console.log('\n' + numero + ' Menor que 0.5 \n\n');
 }
 else {
   console.log('\n' + numero + ' Mayor que 0.5 \n');
